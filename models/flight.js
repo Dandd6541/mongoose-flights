a8ad0459c3f0b4a17ea9779738f1b1e782395e8a
Dandd6541 <Mordkid4@gmail.com>
2022-07-15 14:11:30 -0700

    flights home page

---
+++ models/flight.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+// optional shortcut variable
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+   airline: String,
+   airport: String,
+   flightNo: Number,
+   departs: Date
+});
+
