1d712aa4786e9154751d0bc8b9230d60c696edf6
Dandd6541 <Mordkid4@gmail.com>
2022-07-21 15:38:27 -0700

    final final

--- routes/flights.js
+++ routes/flights.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const flightCtrl = require('../controllers/flights');
-// const { routes } = require('../server');
 
-/* GET users listing. */
 router.get('/', flightCtrl.index);
 router.get('/new', flightCtrl.new);
 router.get('/:id', flightCtrl.show);
